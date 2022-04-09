5e687155ebcbff575d1c6665d4beb039eb2c25b0
pigeonsa1t <bowen_samantha@yahoo.com>
2022-04-09 15:01:26 -0400

    initial push

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,24 @@ $(document).ready(function() {
 
     $("#search-btn").on("click", function() {
        var search = $("#search-city").val()
-       //console.log(search);
-        // navigator.geolocation.getCurrentPosition((position) => {
-        //     console.log(position, position.coords.longitude);
-        //     // use append to get weather into box later:
-        //     var card = $("<div>").addClass("card")
-        //     var lat = $("<h1>").addClass("card-title").text(position.coords.latitude)
-        //     var lon = $("<h1>").addClass("card-title").text(position.coords.longitude)
-        //     $("#current").append(card.append(lat, lon))
+       console.log(search);
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position, position.coords.longitude);
+            // use append to get weather into box later:
+            var card = $("<div>").addClass("card")
+            var lat = $("<h1>").addClass("card-title").text(position.coords.latitude)
+            var lon = $("<h1>").addClass("card-title").text(position.coords.longitude)
+            $("#current").append(card.append(lat, lon))
             
-        // });
-        // drop down for state but search for city
-
-
+        });
+        //drop down for state but search for city
        fetch("http://api.openweathermap.org/geo/1.0/direct?q=Savannah&limit=5&appid=b8ca9a84941951b198c6ce015a8fb6e1").then(function(response) {
            return response.json()
        }).then(function(data) {
-           // LAT & LON WILL ONLY EXIST INSIDE OF HERE WITH
-           // PULL GEOLOCATION FROM BROWSER W STATE DATA?
+        // LAT & LON WILL ONLY EXIST INSIDE OF HERE WITH
+        // PULL GEOLOCATION FROM BROWSER W STATE DATA?
            console.log(data);
-       } )
+       });
     });
 
     //.coords.latitude
